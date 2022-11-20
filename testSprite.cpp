4f42dd6c3774e279d2ecfe7eed9e3ceaed0ee028
Lyle Nelson <Nelsonlylev@yahoo.com>
2022-11-19 19:35:36 -0800

    This is the game character

---
+++ testSprite.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::Texture textureTile;
+    textureTile.loadFromFile("IndianaJonesCanva.png");
+    sf::Sprite prospector;
+    prospector.setTexture(textureTile);
+    // prospector.setTextureRect(sf::IntRect(550, 150, 475, 1400));
+    prospector.setScale (0.25, 0.25);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(prospector);
+        window.display();
+    }
+    return 0;
+}
